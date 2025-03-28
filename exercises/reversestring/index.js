488e05ff5401846c17dbeb5c59b5e0f23c61c44e
vantaylo <vanessalyn.taylor@gmail.com>
2025-03-27 22:25:06 -0500

    reverse str solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// *Solution 1*
+// function reverse(str) {
+// return str.split('').reverse().join('');
+// }
+
+// *Solution 2*
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// } 
+
+// *Solution 3*
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
