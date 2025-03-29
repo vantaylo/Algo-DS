07b649d3345bd1c81a34efbd2c97c990866f8b58
vantaylo <vanessalyn.taylor@gmail.com>
2025-03-28 21:58:40 -0500

    fizzbuzz solutions

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,42 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// *Solution 1*
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if ( i % 3 === 0 && i % 5 === 0 ) {
+//        console.log('fizzbuzz');
+//    } else if ( i % 3 === 0) {
+//        console.log('fizz');
+//    } else if ( i % 5 === 0) {
+//        console.log('buzz');
+//    } else {
+//        console.log(i);
+//    }
+//   }
+// }
+
+// *Solution 2*
+function fizzBuzz(n) {
+  if (n <= 0) {
+    return;
+  }
+
+  fizzBuzz(n - 1);
+
+  let output = '';
+
+  if (n % 3 === 0) {
+    output += 'fizz';
+  }
+  if (n % 5 === 0) {
+    output += 'buzz';
+  }
+  
+  console.log(output || n);
+}
+
+// function fizzBuzz(n) {}
+
 
 module.exports = fizzBuzz;
