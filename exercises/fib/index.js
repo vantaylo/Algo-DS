1543dd8ddbfd39094657b2a355cb7060bb4d8964
vantaylo <vanessalyn.taylor@gmail.com>
2025-04-05 17:00:27 -0500

    fib solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,50 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// *Solution 1*
+// function fib(n) {
+//   let result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i -1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n]
+// }
+
+
+// *Solution 2*
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib( n - 1) + fib(n - 2);
+// }
+
+// *Solution 3*
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      if (cache[args]) {
+        return cache[args];
+      }
+
+      const result = fn.apply(this, args);
+      cache[args] = result;
+      
+      return result;
+    }
+  }
+
+function fib(n) {
+    if (n < 2) {
+    return n;
+  }
+  return fib( n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
