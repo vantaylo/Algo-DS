89cca88b53095bdfde88669f0aba04539bbb0c76
vantaylo <vanessalyn.taylor@gmail.com>
2025-04-07 19:41:53 -0500

    qfroms solution

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,42 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+// *Solution 1*
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(value) { 
+    this.stack1.push(value);
+  }
+  
+  remove() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+    const record = this.stack2.pop();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while(this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    const record = this.stack2.peek();
+
+    while(this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
