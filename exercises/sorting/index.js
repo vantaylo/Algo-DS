29518d1a995359d2ce16d230621111dfad57a22f
vantaylo <vanessalyn.taylor@gmail.com>
2025-05-01 20:52:53 -0500

    selection sort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,7 +3,7 @@
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for ( let j =0; j < (arr.length - i - 1); j++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
       if (arr[j] > arr[j + 1]) {
         const lesser = arr[j + 1];
         arr[j + 1] = arr[j];
@@ -15,8 +15,23 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin= i;
 
-}
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+  
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+    return arr;
+  }
 
 function mergeSort(arr) {
 
